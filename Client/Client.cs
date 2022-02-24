b04c35866f5a7656a8bd51ae69ccf96d94ebb64d
Chowon Jung <56695129+dancing4am@users.noreply.github.com>
2022-02-23 19:02:44 -0500

    initial commit

---
+++ Client/Client.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Disconnecting;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Receiving;
+using MQTTnet.Extensions.ManagedClient;
+
+
+namespace MQTT_using_MQTTnet.Client
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            // create client option builder
+            MqttClientOptionsBuilder builder = new MqttClientOptionsBuilder()
+                .WithClientId("SampleClientID")
+                .WithTcpServer("localhost", 707);
+
+            // create client option
+            ManagedMqttClientOptions options = new ManagedMqttClientOptionsBuilder()
+                                    .WithAutoReconnectDelay(TimeSpan.FromSeconds(60))
+                                    .WithClientOptions(builder.Build())
+                                    .Build();
+
+            // create client
+            IManagedMqttClient client = new MqttFactory().CreateManagedMqttClient();
+
+            // connect handler methods
+            client.ConnectedHandler = new MqttClientConnectedHandlerDelegate(OnConnect);
+            client.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(OnDisconnect);
+            client.ConnectingFailedHandler = new ConnectingFailedHandlerDelegate(OnConnectFail);
+            client.ApplicationMessageReceivedHandler = new MqttApplicationMessageReceivedHandlerDelegate(a => {
+                Console.WriteLine("Message received: {0}", a.ApplicationMessage);
+            });
+
+            // start connect
+            client.StartAsync(options).GetAwaiter().GetResult();
+
+            while (true)
+            {
+                // send message per second
+                string message = "Hello? Anybody there? " + DateTimeOffset.UtcNow;
+                client.PublishAsync("SampleClientID/Topic/Hello", message);
+                Task.Delay(1000).GetAwaiter().GetResult();
+            }
+        }
+
+        public static void OnConnect(MqttClientConnectedEventArgs obj)
+        {
+            Console.WriteLine("Connected to Server");
+        }
+
+        public static void OnConnectFail(ManagedProcessFailedEventArgs obj)
+        {
+            Console.WriteLine("Failed to Connect");
+        }
+
+        public static void OnDisconnect(MqttClientDisconnectedEventArgs obj)
+        {
+            Console.WriteLine("Disconnected from Server");
+        }
+    }
+}
---
+++ MQTT_using_MQTTnet/Server.cs
@@ -0,0 +1,53 @@
+﻿using MQTTnet;
+using MQTTnet.Server;
+using System;
+using System.Text;
+
+namespace MQTT_using_MQTTnet.Broker
+{
+    class Server
+    {
+        private static int MessageCount = 0;    // used as message sequence number
+
+        static void Main(string[] args)
+        {
+            // create server option builder
+            MqttServerOptionsBuilder options = new MqttServerOptionsBuilder()
+                .WithDefaultEndpoint()
+                .WithDefaultEndpointPort(707)
+                .WithConnectionValidator(OnNewConnection)
+                .WithApplicationMessageInterceptor(OnNewMessage);
+
+            // create server
+            IMqttServer server = new MqttFactory().CreateMqttServer();
+
+            // start service
+            server.StartAsync(options.Build()).GetAwaiter().GetResult();
+            Console.ReadLine(); // when any input, stop to debug disconnect
+        }
+
+        public static void OnNewConnection(MqttConnectionValidatorContext context)
+        {
+            // display new client info
+            Console.WriteLine("New Client: ClientID = {0}, Endpoint = {1}, CleanSession = {2}",
+                context.ClientId, context.Endpoint, context.CleanSession);
+        }
+
+        public static void OnNewMessage(MqttApplicationMessageInterceptorContext context)
+        {
+            // prepare message content
+            var payload = context.ApplicationMessage?.Payload == null ? null : Encoding.UTF8.GetString(context.ApplicationMessage?.Payload);
+            MessageCount++;
+
+            // display message in format
+            Console.WriteLine("Msg#: {0} - TimeStamp: {1} -- Message: ClientID = {2}, Topic = {3}, Payload = {4}, QoS = {5}, Retain-Flag = {6}",
+                MessageCount, DateTime.Now,
+                context.ClientId,
+                context.ApplicationMessage?.Topic,
+                payload,
+                context.ApplicationMessage?.QualityOfServiceLevel,
+                context.ApplicationMessage?.Retain
+                );
+        }
+    }
+}
